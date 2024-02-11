9b103b5b1c15816bc101bfb99374ab36c0829208
Kayky Belleboni Casagrande <k.belleboni@edu.pucrs.br>
2024-02-10 22:53:46 -0300

    feat: BrainRust struct

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,30 @@ use std::{
 
 use brain_rust::{interactive::Interactive, program::Program};
 
-fn usage(program_name: String) -> io::Result<()> {
-    eprintln!("Usage: {} [filename | -i]", program_name);
-    exit(1);
-}
+struct BrainRust;
 
-fn interactive() -> io::Result<()> {
-    let program = Program::new(Vec::new());
-    let mut interactive = Interactive::new(program);
+impl BrainRust {
+    fn usage(program_name: String) -> io::Result<()> {
+        eprintln!("Usage: {} [filename | -i]", program_name);
+        exit(1);
+    }
 
-    interactive.run()
-}
+    fn interactive() -> io::Result<()> {
+        let program = Program::new(Vec::new());
+        let mut interactive = Interactive::new(program);
 
-fn run(file_name: &str) -> io::Result<()> {
-    let file = File::open(file_name).unwrap();
-    let mut program = Program::from(file);
+        interactive.run()
+    }
 
-    let mut stdout = stdout();
-    let stdin = stdin();
+    fn run(file_name: &str) -> io::Result<()> {
+        let file = File::open(file_name).unwrap();
+        let mut program = Program::from(file);
 
-    program.run(&mut stdin.lock(), &mut stdout)
+        let mut stdout = stdout();
+        let stdin = stdin();
+
+        program.run(&mut stdin.lock(), &mut stdout)
+    }
 }
 
 fn main() -> io::Result<()> {
@@ -35,8 +39,8 @@ fn main() -> io::Result<()> {
     let program_name = args.next().unwrap();
 
     match args.next().as_deref() {
-        Some("-i") => interactive(),
-        Some(file_name) => run(file_name),
-        _ => usage(program_name),
+        Some("-i") => BrainRust::interactive(),
+        Some(file_name) => BrainRust::run(file_name),
+        _ => BrainRust::usage(program_name),
     }
 }
